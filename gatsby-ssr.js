ea4f96224525b1a24f80ce42ac66b60f9b1ae646
Indrashish Ghosh <mail@indrashishghosh.com>
2020-02-27 19:05:43 +0530

    Added theme hydration

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,37 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require('react')
 
-// You can delete this file if you're not using it
+exports.onRenderBody = function ({ setPreBodyComponents }) {
+  setPreBodyComponents([
+    React.createElement('script', {
+      key: 'daark-mode-baby',
+      dangerouslySetInnerHTML: {
+        __html: `
+void function() {
+  window.__onThemeChange = function() {}
+  var preferredTheme
+  try {
+    preferredTheme = localStorage.getItem('canIHazDarkMode')
+  } catch (err) { }
+  function setTheme(newTheme) {
+    window.__theme = newTheme
+    preferredTheme = newTheme
+    document.body.className = newTheme
+    window.__onThemeChange(newTheme)
+  }
+  window.__setPreferredTheme = function(newTheme) {
+    setTheme(newTheme)
+    try {
+      localStorage.setItem('canIHazDarkMode', newTheme)
+    } catch (err) {}
+  }
+  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+  darkQuery.addListener(function(e) {
+    window.__setPreferredTheme(e.matches ? 'dark' : 'light')
+  })
+  setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'))
+}()
+    `
+      }
+    })
+  ])
+}
