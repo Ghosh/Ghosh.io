865acea863a07e719130c9263e88df27b5991ccd
Indrashish Ghosh <mail@indrashishghosh.com>
2020-08-09 20:54:34 +0530

    chore: Update commitzen config

---
+++ changelog.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "disableEmoji": false,
+  "list": [
+    "refactor",
+    "style",
+    "feat",
+    "chore",
+    "fix",
+    "docs",
+    "ci",
+    "test",
+    "perf"
+  ],
+  "questions": [
+    "type",
+    "subject",
+  ],
+
+}
+
