3848b58d6f5daf45c21a2cf42f68a5612c73b3a4
Indrashish Ghosh <mail@indrashishghosh.com>
2020-08-12 09:53:04 +0530

    style: Added styled components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${ ({ theme }) => theme.colors.primary };
+`
 
 const Home = () => {
-  return <div>Welcome to Next.js!</div>
+  return <Title>Welcome to Next.js!</Title>
 }
 
 export default Home
