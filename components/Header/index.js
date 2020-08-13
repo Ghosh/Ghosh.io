5401692278712ed3e9da4184906d5bd7407e2e6e
Indrashish Ghosh <mail@indrashishghosh.com>
2020-08-13 21:41:03 +0530

    feat: Added Nav to header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
-import Wrapper from 'components/Wrapper'
 import Box from '@stylekit/ui/Box'
 import Flex from '@stylekit/ui/Flex'
+
+import Wrapper from 'components/Wrapper'
+
+import Nav from './Nav'
+import Logo from './svg/logo.svg'
 
 const Header = () => {
   return (
     <Wrapper>
-      <Box height="80px" width="100%" vCentered>
+      <Box height="140px" width="100%" vCentered>
         <Flex justifyContent="between">
           <Flex>
-            <div>Logo</div>
-            <div>Nav</div>
+            <Logo width="65" />
+            <Nav />
           </Flex>
 
           <Flex.Box alignItems="end">
