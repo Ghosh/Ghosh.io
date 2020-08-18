a276b670f89de244b58a759992f5666e5f24fc3f
Indrashish Ghosh <mail@indrashishghosh.com>
2020-08-18 15:58:57 +0530

    style: Moved hero type to serif

--- modules/Home/Hero/index.js
+++ modules/Home/Hero/index.js
@@ -8,7 +8,7 @@ const Hero = () => {
     <>
       <Spacer mt={['70px', '80px', '100px']} />
 
-      <Text fontSize={['24px', '30px']} m="0">
+      <Text fontSize={['18px', '24px', '30px']} m="0">
         <span role="img" aria-label="wave">
           👋
         </span>
@@ -17,31 +17,35 @@ const Hero = () => {
       <Spacer mt={['3', '3', '4']} />
 
       <Text
-        fontSize={['h700', 'h800', 'h900']}
-        fontWeight="600"
+        fontSize={['h600', 'h700', 'h800', 'h900']}
+        fontWeight="500"
+        fontFamily="serif"
+        width={['100%', '95%', '90%', '85%', '80%']}
+        lineHeight="1.4"
         m="0"
         css={`
           color: var(--text-1-color);
         `}
       >
-        Hi, I am Indrashish Ghosh
+        Hi, I am Indrashish Ghosh. A product designer who enjoys designing and
+        building experiences for the next billion users
       </Text>
 
-      <Spacer my={['2', '3']} />
+      {/* <Spacer my={['2', '3']} />
 
       <Text
         fontSize={['h600', 'h600', 'h700']}
         lineHeight={['2.9rem', '2.9rem', '4.2rem']}
-        fontWeight="400"
-        width={['100%', '85%', '53%']}
+        fontWeight="300"
+        width={['100%', '90%', '75%', '60%', '53%']}
         m="0"
         css={`
           color: var(--text-2-color);
         `}
       >
-        I am a Product Designer who enjoys designing and building experiences
+        A product designer who enjoys designing and building experiences
         for the next billion users
-      </Text>
+      </Text> */}
 
       <Spacer mb="7" />
     </>
