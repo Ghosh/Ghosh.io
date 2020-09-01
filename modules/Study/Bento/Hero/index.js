55ec3201f6bddaa584598fe533d046bdedaece7c
Indrashish Ghosh <mail@indrashishghosh.com>
2020-09-02 01:37:42 +0530

    refactor: Updated mdx workflow

---
+++ modules/Study/Bento/Hero/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import Text from '@stylekit/ui/Text'
+import Box from '@stylekit/ui/Box'
+
+import Wrapper from 'components/Wrapper'
+import Header from 'components/Header'
+
+const Hero = () => {
+  return (
+    <Box
+      height="640px"
+      bg="var(--bg-2)"
+    >
+      <Wrapper>
+        <Header />
+        <Text
+          as="h1"
+          fontSize={['h500', 'h700', 'h900']}
+          lineHeight={['2.9rem', '3.4rem', '4.6rem']}
+          fontWeight="500"
+          width={['100%', '90%', '75%', '50%']}
+          mt={['6']}
+          mb={['3']}
+          color="var(--text-1-color)"
+        >
+          Establishing a design system for Cleartrip
+        </Text>
+        <Text
+          as="h2"
+          fontSize={['h600', 'h700', '2.5rem']}
+          lineHeight={['2.9rem', '3.4rem', '4.2rem']}
+          fontWeight="300"
+          width={['100%', '90%', '75%', '50%']}
+          m="0"
+          color="var(--text-2-color)"
+        >
+          I spent two years at Cleartrip setting up the design systems team
+        </Text>
+      </Wrapper>
+    </Box>
+  )
+}
+
+export default Hero
---
+++ pages/work/bento/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Master from 'layouts/Master'
+import Wrapper from 'components/Wrapper'
+import Hero from 'modules/Study/Bento/Hero'
+
+import Content from './bento.mdx'
+
+const Bento = () => {
+  return (
+    <Master>
+      <Hero />
+      <Wrapper>
+        <Content />
+      </Wrapper>
+    </Master>
+  )
+}
+
+export default Bento
